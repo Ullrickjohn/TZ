a3953289074d5caff4f331160b09ce933a1783cd
John <ullrickjohn@gmail.com>
2023-10-30 00:08:20 +0500

    feat: connected backend with api and complated all tasks and solved all issues

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ const cardsWrapper = document.querySelector(".card-block")
 const api = "https://dev-cabinet.seenday.com/e.scripts?page=pages:unload&event=get&unload_id=2175"
 
 const endpoints = {
-data: 'api',
+date: 'api',
 status: 'api',
 id: 'api'
 }
-function inch-data(api){
+function inchData(api){
 return `
    
 <div class="block">
-<div>Задача выполнена: <span class="text-bold"> ${api.data}</span></div>
+<div>Задача выполнена: <span class="text-bold"> ${api.date}</span></div>
 <div>Статус задачи: <span class="text-bold"> ${api.status}</span></div>
 <div>ID выгрузки: <span class="text-bold">${api.id}</span> </div>
 </div> 
@@ -20,7 +20,7 @@ fetch(api + endpoints.data)
 .then(res => res.json())
 .then(data => {
 data.response.data.forEach(inch =>{
-cardsWrapper.innerHTML += inch(inch)
+cardsWrapper.innerHTML += inchData(inch)
 
 })
 })
